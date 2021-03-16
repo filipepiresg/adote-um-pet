5ba3b0c5ac10ebb26acb3dc1eb4f13c9e8c2a9ca
Filipe Pires <filipepiresg@gmail.com>
2021-03-15 21:46:06 -0300

    feat: add page

--- src/pages/Add/index.js
+++ src/pages/Add/index.js
@@ -1,14 +1,167 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
-import { Header } from '~/src/components';
+import firestore from '@react-native-firebase/firestore';
 
-import { Container } from './styles';
+import { useFormik } from 'formik';
+import { get } from 'lodash';
+import * as Yup from 'yup';
 
-const Add = () => (
-  <>
-    <Header title='Adicionar pet' hasLeftIcon={false} />
-    <Container />
-  </>
-);
+import { Header, Input, Button } from '~/src/components';
+
+import Styles, { Container } from './styles';
+
+const SCHEMA = Yup.object({
+  name: Yup.string()
+    .trim('Esse campo não pode ser só espacos')
+    .required('Esse campo é obrigatório'),
+  type: Yup.string().required('Esse campo é obrigatório'),
+  age: Yup.number()
+    .nullable('Idade deve ser preenchida')
+    .min(0, 'Abaixo da idade mínima')
+    .max(100, 'Ultrapassa a idade máxima')
+    .required('Esse campo é obrigatório'),
+  breed: Yup.string().required('Esse campo é obrigatório'),
+  description: Yup.string(),
+});
+
+const Add = () => {
+  const [loading, setLoading] = useState(false);
+
+  const nameRef = useRef();
+  const typeRef = useRef();
+  const ageRef = useRef();
+  const breedRef = useRef();
+  const descriptionRef = useRef();
+
+  const handleSubmit = useCallback((values, { resetForm }) => {
+    setLoading(true);
+
+    firestore()
+      .collection('pets')
+      .add({
+        ...values,
+        description: get(values, 'description', 'Não possui descrição'),
+      })
+      .then(() => {
+        resetForm();
+      })
+      .catch((error) => {
+        console.log('Error on create pet', error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      type: '',
+      age: null,
+      breed: '',
+      description: '',
+    },
+    onSubmit: handleSubmit,
+    enableReinitialize: true,
+    validationSchema: SCHEMA,
+  });
+
+  return (
+    <>
+      <Header title='Adicionar pet' hasLeftIcon={false} />
+      <Container>
+        <KeyboardAvoidingView behavior='padding' enabled={Platform.OS === 'ios'}>
+          <Input
+            editable={!loading}
+            ref={typeRef}
+            title='Tipo de animal'
+            styleInput={Styles.input}
+            value={formik.values.type}
+            error={formik.touched.type ? formik.errors.type : undefined}
+            onChangeText={formik.handleChange('type')}
+            onSubmitEditing={() => {
+              nameRef.current?.focus();
+            }}
+            autoCapitalize='sentences'
+            autoCorrect
+            keyboardType='default'
+            placeholder='Tipo do pet'
+            returnKeyType='next'
+          />
+
+          <Input
+            editable={!loading}
+            ref={nameRef}
+            title='Nome'
+            styleInput={Styles.input}
+            value={formik.values.name}
+            error={formik.touched.name ? formik.errors.name : undefined}
+            onChangeText={formik.handleChange('name')}
+            onSubmitEditing={() => {
+              ageRef.current?.focus();
+            }}
+            autoCapitalize='words'
+            autoCorrect={false}
+            keyboardType='default'
+            placeholder='Nome do pet'
+            returnKeyType='next'
+          />
+
+          <Input
+            editable={!loading}
+            ref={ageRef}
+            title='Idade'
+            styleInput={Styles.input}
+            value={formik.values.age}
+            error={formik.touched.age ? formik.errors.age : undefined}
+            onChangeText={formik.handleChange('age')}
+            onSubmitEditing={() => {
+              breedRef.current?.focus();
+            }}
+            keyboardType='numeric'
+            placeholder='Idade do pet (em anos)'
+            returnKeyType='next'
+          />
+
+          <Input
+            editable={!loading}
+            ref={breedRef}
+            title='Raça'
+            styleInput={Styles.input}
+            value={formik.values.breed}
+            error={formik.touched.breed ? formik.errors.breed : undefined}
+            onChangeText={formik.handleChange('breed')}
+            onSubmitEditing={() => {
+              descriptionRef.current?.focus();
+            }}
+            autoCapitalize='sentences'
+            autoCorrect
+            keyboardType='default'
+            placeholder='Raça do pet'
+            returnKeyType='next'
+          />
+
+          <Input
+            editable={!loading}
+            ref={descriptionRef}
+            title='Descrição'
+            styleInput={Styles.input}
+            value={formik.values.description}
+            error={formik.touched.description ? formik.errors.description : undefined}
+            onChangeText={formik.handleChange('description')}
+            onSubmitEditing={formik.submitForm}
+            autoCapitalize='sentences'
+            autoCorrect
+            keyboardType='default'
+            placeholder='Descrição do pet'
+            returnKeyType='send'
+          />
+        </KeyboardAvoidingView>
+        <Button onPress={formik.submitForm}>Enviar dados</Button>
+      </Container>
+    </>
+  );
+};
 
 export default Add;
--- src/pages/Add/styles.js
+++ src/pages/Add/styles.js
@@ -1,8 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+import { transparentize } from 'polished';
 import styled from 'styled-components/native';
 
 import { Colors } from '~/src/utils';
 
-export const Container = styled.View`
-  flex: 1;
+export const Container = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  keyboardShouldPersistTaps: 'handled',
+})`
   background-color: ${Colors.BACKGROUND};
+  padding: 10px 10px 20px;
 `;
+
+export default StyleSheet.create({
+  input: {
+    backgroundColor: transparentize(0.9, Colors.BLACK),
+  },
+});
