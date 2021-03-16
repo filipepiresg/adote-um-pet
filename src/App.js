d9133aa3561612d521bcc899ec6ebf0fbdab3a89
Filipe Pires <filipepiresg@gmail.com>
2021-03-16 04:24:43 -0300

    feat: status bar configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './config/ReactotronConfig';
 
 import React from 'react';
-import { LogBox } from 'react-native';
+import { LogBox, Platform, StatusBar } from 'react-native';
 import Config from 'react-native-config';
 import Geocoder from 'react-native-geocoding';
 
 import { UserProvider } from '~/src/contexts/user';
 
 import Routes from './routes';
+import { Colors } from './utils';
 
 Geocoder.init(Config.GOOGLE_MAPS_API_KEY, { language: 'pt-BR' });
 
@@ -15,8 +16,14 @@ LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <UserProvider>
-      <Routes />
-    </UserProvider>
+    <>
+      <StatusBar
+        backgroundColor={Colors.PRIMARY}
+        barStyle={Platform.OS === 'ios' ? 'dark-content' : 'dark-content'}
+      />
+      <UserProvider>
+        <Routes />
+      </UserProvider>
+    </>
   );
 }
