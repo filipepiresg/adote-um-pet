2aa7916cc47ef2452ce1db14fd21e306e9affb9d
Filipe Pires <filipepiresg@gmail.com>
2021-03-16 04:09:29 -0300

    chore: icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,4 @@
+export { default as Logo } from './logo.svg';
+export { default as LogoColored } from './logo-colored.svg';
+export { default as Home } from './home.svg';
+export { default as Pets } from './pet.svg';
